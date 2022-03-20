4dec0a7034928323e51732c4ef52719c3c9a8076
filiperobotic <filipe.rolim@gmail.com>
2022-03-20 17:11:26 -0300

    gradcam

---
+++ notebooks/torchcam/tests/test_methods_utils.py
@@ -0,0 +1,27 @@
+from torchvision.models import resnet18
+
+from torchcam.methods import _utils
+
+
+def test_locate_candidate_layer(mock_img_model):
+    # ResNet-18
+    mod = resnet18().eval()
+    assert _utils.locate_candidate_layer(mod) == 'layer4'
+
+    # Custom model
+    mod = mock_img_model.train()
+
+    assert _utils.locate_candidate_layer(mod) == '0.3'
+    # Check that the model is switched back to its origin mode afterwards
+    assert mod.training
+
+
+def test_locate_linear_layer(mock_img_model):
+
+    # ResNet-18
+    mod = resnet18().eval()
+    assert _utils.locate_linear_layer(mod) == 'fc'
+
+    # Custom model
+    mod = mock_img_model
+    assert _utils.locate_linear_layer(mod) == '2'
